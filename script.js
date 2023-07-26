d5819993638d80e058fccf93d3146664b4a41ac5
abdul Gafaru <kenneth.danso@mybestquote.com>
2023-07-26 00:59:42 +0000

    stored fav city weather to local storage

--- script.js
+++ script.js
@@ -3,12 +3,11 @@
 const apiKey = "c57ba05eb97fd445061f54a80d1a7ca7";
 const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
 
-const temperature = document.querySelector(".temperature");
-const myCity = document.querySelector(".location");
-const humidity = document.querySelector(".humidity");
-const wind = document.querySelector(".wind");
-const weather = document.querySelector(".weather");
-const country = document.querySelector(".country");
+let temperature = document.querySelector(".temperature");
+let myCity = document.querySelector(".location");
+let humidity = document.querySelector(".humidity");
+let wind = document.querySelector(".wind");
+let weather = document.querySelector(".weather");
 const searchInput = document.querySelector(".searchInput");
 const searchWeather = document.querySelector(".search");
 
@@ -16,13 +15,22 @@ const searchWeather = document.querySelector(".search");
 async function fetchWeatherData(city) {
   const response = await fetch(url + city + `&appid=${apiKey}`);
     let data = await response.json();
-    
+
     weather.textContent += data.weather[0].description;
     temperature.textContent += Math.round(data.main.temp) + " °C";
-    myCity .textContent += data.name;
-   country .textContent += data.sys.country;
+    let country = data.sys.country;
+    myCity.textContent += data.name + `, ${country}`;
     humidity.textContent += data.main.humidity + "%";
     wind.textContent += data.wind.speed + " km/h";
+    
+  return {
+    weather: data.weather[0].description,
+    temperature: Math.round(data.main.temp),
+    city: data.name,
+    country: country,
+    humidity: data.main.humidity,
+    wind: data.wind.speed,
+  };
   }
   
   searchWeather.addEventListener("click", () =>{
@@ -30,10 +38,23 @@ async function fetchWeatherData(city) {
     searchInput.value = "";
   })
 
- const myFav = [];
-console.log(myFav);
- const favBtn = document.querySelector(".favBtn");
+  const myFav = JSON.parse(localStorage.getItem("favorites")) || [];
 
- favBtn.addEventListener("click", () => {
-  console.log(myFav.push(fetchWeatherData()));
- })
+//  const myFav = [];
+const favBtn = document.querySelector(".favBtn");
+
+favBtn.addEventListener("click", async () => {
+  const city = searchInput.value;
+  const weatherData = await fetchWeatherData(city);
+  myFav.push(weatherData);
+  localStorage.setItem("favorites", JSON.stringify(myFav));
+  searchInput.value = "";
+});
+
+
+const myFavCity = () => {
+  
+  // get data from local storage 
+
+  // display data on my favourites
+}

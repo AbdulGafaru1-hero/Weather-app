e6b3af8671f937b670c497c4e017e8e8bbc382ef
abdul Gafaru <kenneth.danso@mybestquote.com>
2023-07-26 03:45:32 +0000

    Completed the favourites Ui

--- script.js
+++ script.js
@@ -3,58 +3,93 @@
 const apiKey = "c57ba05eb97fd445061f54a80d1a7ca7";
 const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
 
-let temperature = document.querySelector(".temperature");
-let myCity = document.querySelector(".location");
-let humidity = document.querySelector(".humidity");
-let wind = document.querySelector(".wind");
-let weather = document.querySelector(".weather");
+const temperature = document.querySelector(".temperature");
+const myCity = document.querySelector(".location");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const weather = document.querySelector(".weather");
+const fouvoriteUi = document.querySelector(".favourite");
 const searchInput = document.querySelector(".searchInput");
 const searchWeather = document.querySelector(".search");
 
 
+
 async function fetchWeatherData(city) {
-  const response = await fetch(url + city + `&appid=${apiKey}`);
-    let data = await response.json();
-
-    weather.textContent += data.weather[0].description;
-    temperature.textContent += Math.round(data.main.temp) + " °C";
-    let country = data.sys.country;
-    myCity.textContent += data.name + `, ${country}`;
-    humidity.textContent += data.main.humidity + "%";
-    wind.textContent += data.wind.speed + " km/h";
-    
-  return {
-    weather: data.weather[0].description,
-    temperature: Math.round(data.main.temp),
-    city: data.name,
-    country: country,
-    humidity: data.main.humidity,
-    wind: data.wind.speed,
-  };
-  }
-  
-  searchWeather.addEventListener("click", () =>{
-    fetchWeatherData(searchInput.value)
-    searchInput.value = "";
-  })
-
   const myFav = JSON.parse(localStorage.getItem("favorites")) || [];
+  try {
+    const response = await fetch(url + city + `&appid=${apiKey}`);
+    const data = await response.json();
 
-//  const myFav = [];
+  const weatherData = {
+      weather: data.weather[0].description,
+      temperature: Math.round(data.main.temp),
+      myCity: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      wind: data.wind.speed,
+    };
+
+    weather.textContent = weatherData.weather;
+    temperature.textContent = weatherData.temperature + " °C";
+    myCity.textContent += weatherData.myCity + ", " + weatherData.country;
+    humidity.textContent = weatherData.humidity + "%";
+    wind.textContent = weatherData.wind + " km/h";
+    localStorage.setItem("favorites", JSON.stringify(myFav));
+    return weatherData;
+
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    throw error;
+  }};
+
+  searchWeather.addEventListener("click", () => {
+    if (searchInput.value === "") {
+      alert("please enter a city name")
+      return
+    } 
+    fetchWeatherData(searchInput.value);
+    fetchWeatherData(weatherData);
+    searchInput.value = "";
+  });
+
 const favBtn = document.querySelector(".favBtn");
 
 favBtn.addEventListener("click", async () => {
+  const myFav = JSON.parse(localStorage.getItem("favorites")) || [];
   const city = searchInput.value;
-  const weatherData = await fetchWeatherData(city);
-  myFav.push(weatherData);
-  localStorage.setItem("favorites", JSON.stringify(myFav));
-  searchInput.value = "";
+  try {
+    const weatherData = await fetchWeatherData(city);
+    myFav.push(weatherData);
+    localStorage.setItem("favorites", JSON.stringify(myFav));
+    searchInput.value = "";
+  } catch (error) {
+    console.error('Error fetching and adding weather data to favorites:', error);
+  }
 });
-
-
 const myFavCity = () => {
-  
-  // get data from local storage 
+  const myFav = JSON.parse(localStorage.getItem("favorites")) || [];
+  fouvoriteUi.innerHTML = ""; 
 
-  // display data on my favourites
+const favWeather = myFav.forEach(element => {
+  fouvoriteUi.innerHTML += ` <div class="fav">
+  <h3 class="close">x</h3>
+  <div class="countryText">
+  <h4>Weather of ${element.myCity}, ${element.country}</h4> </div>
+  <div class="elements"><div class="new">
+<h2 class="temperature tem2">${element.temperature}°C</h2>
+<p >Temperature</p> 
+  </div>
+<div class="new">
+  <h2 class="humidity">${element.humidity}%</h2> 
+  <p >Humidity</p> 
+</div>
+
+<div class="new">
+  <h2 class="wind">${element.wind}km/h</h2> 
+  <p >wind Speed</p> 
+</div>
+</div>
+</div>`
+});
 }
+myFavCity();
